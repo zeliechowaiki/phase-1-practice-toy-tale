117ecf5a02e97a85e909e1a8473db8aa6e11c49f
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-02-09 17:25:05 -0500

    commit tree 5655225ddb227c280113672bdbb3a0e8fa285d78

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 let addToy = false;
+const toyCollection = document.getElementById('toy-collection');
+const form = document.querySelector('.add-toy-form');
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +15,74 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function getToys() {
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(toys => toys.map(toy => makeCard(toy)));
+}
+getToys();
+
+function makeCard(toy) {
+  let toyDiv = document.createElement('div');
+  toyDiv.classList.add('card');
+  let toyName = document.createElement('h2');
+  toyName.textContent = toy.name;
+  let toyImg = document.createElement('img');
+  toyImg.src = toy.image;
+  toyImg.classList.add('toy-avatar');
+  let toyLikes = document.createElement('p');
+  toyLikes.textContent = `${toy.likes} Likes`
+  let toyButton = document.createElement('button');
+  toyButton.classList.add('like-btn');
+  toyButton.id = toy.id;
+  toyButton.textContent = 'Like';
+
+  toyDiv.appendChild(toyName);
+  toyDiv.appendChild(toyImg);
+  toyDiv.appendChild(toyLikes);
+  toyDiv.appendChild(toyButton);
+
+  toyCollection.appendChild(toyDiv);
+
+  toyButton.addEventListener('click', () => {
+    ++toy.likes;
+    toyLikes.textContent = `${toy.likes} Likes`;
+    patchLikes(toy, toy.likes);
+  });
+}
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  let newToyName = (e.target.name.value);
+  let newToyImg = (e.target.image.value);
+  fetch('http://localhost:3000/toys',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      'name': newToyName,
+      'image': newToyImg,
+      'likes': 0
+    })
+  })
+  .then(resp => resp.json())
+  .then(toy => makeCard(toy));
+})
+
+function patchLikes(toy,likes) {
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: "PATCH",
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      'likes': likes
+    })
+  })
+  .then(resp => resp.json())
+  .then(card => console.log(card));
+}
